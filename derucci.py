3835538494af85be238e48f5b262458a187dcdad
Daenerys <154054509@qq.com>
2021-10-24 18:08:11 +0800

    10.20

--- derucci.py
+++ derucci.py
@@ -1,6 +1,10 @@
+from telnetlib import EC
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
 driver = webdriver.Chrome()
 driver.get("https://login.derucci-smart.com/dashboard")
@@ -12,23 +16,33 @@ login = driver.find_element_by_name("password")
 login.clear()
 login.send_keys("111111")
 login = driver.find_element_by_xpath("//button[@class='el-button ms-submit el-button--primary el-button--medium']")
-
-#get current page handle name
-current_window = driver.current_window_handle
-#login to baskboard
 login.send_keys(Keys.ENTER)
 #inter the page of dashboard
-assert "No results found." not in driver.page_source
+
+jumpPage = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[2]/div[2]'))
+    )
+jumpPage.click()
+
+# sleepRange = WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[1]/div/ul/div[5]/li/div/div/span'))
+# )
+# sleepRange.click()
+# assert "慕思智能云平台" in driver.page_source
+# operatPage = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]/div[1]/div/ul/div[7]/li/div')
+operatPage = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[2]/div[2]/div[1]/div/ul/div[7]/li/div/i'))
+)
+operatPage.click()
+# driver.quit()
 #focus on current page
-all_window = driver.window_handles
-for window in all_window:
-    if window != current_window:
-        driver.switch_to.window(window)
-#get current page handle name
-current_window = driver.current_window_handle
-elem = driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div[2]')
-# print(elem)
-elem.click()
+# all_window = driver.window_handles
+# for window in all_window:
+#     if window != current_window:
+#         driver.switch_to.window(window)
+# #get current page handle name
+# current_window = driver.current_window_handle
+
 # elem = driver.find_elements_by_css_selector("div.xh-highlight")
 # ActionChainsDriver = ActionChains(driver).click(elem)
 # ActionChainsDriver.perform()
